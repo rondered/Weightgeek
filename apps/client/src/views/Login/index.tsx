a31b135d3544eb9c3a50c313ee33e7193f58700a
Ron Mizrahi <sronmiz@gmail.com>
2021-08-15 10:46:28 +0300

    fixes

--- apps/client/src/views/Login/index.tsx
+++ apps/client/src/views/Login/index.tsx
@@ -8,19 +8,15 @@ import { HiOutlineMail as MailIcon } from "react-icons/hi";
 import { RiLockPasswordLine as PasswordIcon } from "react-icons/ri";
 
 const LoginContainer = styled.div`
-  ${tw`flex h-screen justify-center items-center md:justify-center md:w-1/2`}
+  ${tw`flex h-screen w-full justify-center items-center md:justify-center md:w-1/2`}
 `;
 
 const LoginFormContainer = styled.div`
-  ${tw`w-full h-screen p-10 md:w-1/2 md:h-auto`}
+  ${tw`w-full h-screen p-10 md:w-1/2 md:min-w-[400px] md:h-auto`}
 `;
 
 const LoginHeader = styled.div`
   ${tw`mb-8 text-4xl font-extrabold text-center`}
-`;
-
-const DividerContainer = styled.div`
-  ${tw`flex mt-6 mb-6`}
 `;
 
 const InputField = styled.input<{ isInvalid: boolean }>`
@@ -56,8 +52,6 @@ const OfferSignupContainer = styled.div`
   ${tw`mt-10`}
 `;
 
-const SuccessRedirect: React.FC<{}> = () => <Redirect to="/" />;
-
 export const Login = () => {
   const {
     isLoading,
@@ -72,56 +66,61 @@ export const Login = () => {
   return (
     <MainContainer>
       {isSuccess && <Redirect to="/" />}
-      <LoginContainer>
-        <LoginFormContainer>
-          <LoginHeader>Welcome Back!</LoginHeader>
-          <div css={tw`flex gap-10 justify-center`}>
-            <a href="http://localhost:4444/auth/google/redirect">
-              <SocialLoginButton variation="google" />
-            </a>
-            <a href="http://localhost:4444/auth/facebook/redirect">
-              <SocialLoginButton variation="facebook" />
-            </a>
-          </div>
-          <DividerContainer>
-            <FormDivider text="OR" />
-          </DividerContainer>
-          <form onSubmit={handleSubmit}>
-            <FormContainer>
-              <InputContainer>
-                <Input
-                  {...register("email")}
-                  type="email"
-                  name="email"
-                  placeholder="email"
-                  isInvalid={formErrors.email}
-                  icon={<MailIcon size="20px" />}
-                />
-                <InputError>
-                  {formErrors.email && <p>{formErrors.email.message}</p>}
-                </InputError>
-              </InputContainer>
-              <InputContainer>
-                <Input
-                  {...register("password")}
-                  type="password"
-                  name="password"
-                  placeholder="password"
-                  isInvalid={formErrors?.password}
-                  icon={<PasswordIcon size="20px" />}
-                />
-                <InputError>
-                  {formErrors.password && <p>{formErrors.password.message}</p>}
-                </InputError>
-              </InputContainer>
-              <FormButton isLoading={isLoading} text="Login" />
-            </FormContainer>
-          </form>
-          <OfferSignupContainer>
-            Don't have an account? <Link to="/signup">Signup</Link>
-          </OfferSignupContainer>
-        </LoginFormContainer>
-      </LoginContainer>
+      <div css={tw`flex w-full`}>
+        <LoginContainer>
+          <LoginFormContainer>
+            <LoginHeader>Welcome Back!</LoginHeader>
+            <div css={tw`flex gap-10 justify-center`}>
+              <a href="http://localhost:4444/auth/google/redirect">
+                <SocialLoginButton variation="google" />
+              </a>
+              <a href="http://localhost:4444/auth/facebook/redirect">
+                <SocialLoginButton variation="facebook" />
+              </a>
+            </div>
+            <div css={tw`flex mt-6 mb-6`}>
+              <FormDivider text="OR" />
+            </div>
+            <form onSubmit={handleSubmit}>
+              <FormContainer>
+                <InputContainer>
+                  <Input
+                    {...register("email")}
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    isInvalid={formErrors.email}
+                    icon={<MailIcon size="20px" />}
+                  />
+                  <InputError>
+                    {formErrors.email && <p>{formErrors.email.message}</p>}
+                  </InputError>
+                </InputContainer>
+                <InputContainer>
+                  <Input
+                    {...register("password")}
+                    type="password"
+                    name="password"
+                    placeholder="password"
+                    isInvalid={formErrors?.password}
+                    icon={<PasswordIcon size="20px" />}
+                  />
+                  <InputError>
+                    {formErrors.password && (
+                      <p>{formErrors.password.message}</p>
+                    )}
+                  </InputError>
+                </InputContainer>
+                <FormButton isLoading={isLoading} text="Login" />
+              </FormContainer>
+            </form>
+            <OfferSignupContainer>
+              Don't have an account? <Link to="/signup">Signup</Link>
+            </OfferSignupContainer>
+          </LoginFormContainer>
+        </LoginContainer>
+        <div css={tw`hidden md:block md:w-1/2`}>lala</div>
+      </div>
     </MainContainer>
   );
 };
