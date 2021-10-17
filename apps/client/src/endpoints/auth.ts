c8c1d46752538d05fef078647d2a8873fe3fafdd
Ron Mizrahi <sronmiz@gmail.com>
2021-10-17 09:46:02 +0300

    fix

--- apps/client/src/endpoints/auth.ts
+++ apps/client/src/endpoints/auth.ts
@@ -1,4 +1,5 @@
 import {axiosInstance} from "@/utils";
+import { SignUpUserDto, LoginUserDto } from '@wg/types';
 
 const PATH = "auth";
 
@@ -12,12 +13,12 @@ export const getLogout = async (values: any) => {
   return data;
 };
 
-export const postLogin = async (values: any) => {
+export const postLogin = async (values: LoginUserDto) => {
   const {data} = await axiosInstance.post(`${PATH}/login`, values);
   return data;
 };
 
-export const postSignUp = async (values: any) => {
+export const postSignUp = async (values: SignUpUserDto) => {
   const {data} = await axiosInstance.post(`${PATH}/signup`, values);
   return data;
 };
--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -4,12 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import {CustomQueryClientProvider} from "@/providers";
-import { SignUpUserDto } from '@wg/types';
 
-const lala : SignUpUserDto = {
-  email: 'lala@lala.com'
-}
-console.log(lala);
 ReactDOM.render(
   <React.StrictMode>
     <CustomQueryClientProvider>
