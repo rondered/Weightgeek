04c557e34200d2f15c1a9e7d75555cdabd7aca6c
sronmiz@gmail.com <sronmiz@gmail.com>
2021-10-23 18:04:51 +0300

    fix workspace

--- apps/api/src/types/dtos/add-weight-in.dto.ts
+++ apps/api/src/types/dtos/add-weight-in.dto.ts
@@ -1,11 +1,13 @@
-import { IsNotEmpty, IsDateString } from 'class-validator';
+import { IsNotEmpty, IsDateString, IsPositive } from 'class-validator';
 
 export class AddWeightInDto {
+  @IsPositive()
   weight: number;
 
   @IsNotEmpty()
   @IsDateString()
   date: Date;
 
+  @IsPositive()
   calories?: number;
 }
--- apps/client/src/endpoints/auth.ts
+++ apps/client/src/endpoints/auth.ts
@@ -1,5 +1,4 @@
 import {axiosInstance} from "@/utils";
-import { SignUpUserDto, LoginUserDto } from '@wg/types';
 
 const PATH = "auth";
 
@@ -13,12 +12,12 @@ export const getLogout = async (values: any) => {
   return data;
 };
 
-export const postLogin = async (values: LoginUserDto) => {
+export const postLogin = async (values: any) => {
   const {data} = await axiosInstance.post(`${PATH}/login`, values);
   return data;
 };
 
-export const postSignUp = async (values: SignUpUserDto) => {
+export const postSignUp = async (values: any) => {
   const {data} = await axiosInstance.post(`${PATH}/signup`, values);
   return data;
 };
